7343cb8d0e58d05b6ade41d0c2b2d0179dd4b353
FourwingsY <fourwingsy@gmail.com>
2015-01-20 18:27:56 +0900

    first refactorization fin

--- webapp/script/App.js
+++ webapp/script/App.js
@@ -46,22 +46,11 @@ app.controller("MapController", ['$scope', 'dataService', function($scope, dataS
 
 		this.currentCityName = city.name;
 		// reset region hover data
-		for (var i in $scope.data.regions) {
-			var r = $scope.data.regions[i];
-			r.hover = false;
-		}
+		this.resetHover();
 
 		city.hover = true;
 		region.hover = true;
 	}.bind(this);
-
-	$scope.out = function(cityId, regionId) {
-		var city = $scope.data.cities[cityId];
-		// var region = $scope.data.regions[regionId];
-		city.hover = false;
-		// region.hover = false;
-		console.log(this);
-	};
 
 	$scope.click = function(cityId){
 		var city = $scope.data.cities[cityId];
@@ -70,6 +59,17 @@ app.controller("MapController", ['$scope', 'dataService', function($scope, dataS
 		var checkbox = document.querySelector("#cb_"+cityId);
 		checkbox.checked = city.selected;
 	};
+
+	this.resetHover = function() {
+		for (var i in $scope.data.cities) {
+			var c = $scope.data.cities[i];
+			c.hover = false;
+		}
+		for (var j in $scope.data.regions) {
+			var r = $scope.data.regions[j];
+			r.hover = false;
+		}
+	}
 }]);
 
 app.filter('citiesInRegion', function() {
@@ -82,4 +82,56 @@ app.filter('citiesInRegion', function() {
 		});
 		return filtered;
 	};
-})
+});
+
+app.controller("ZoomController", function() {
+
+	this.move = function(e) {
+		var coordinate = this.getCursorXY(e);
+		this.zoomBox = this.getViewBox(coordinate, 2);
+		var zoomPos = this.getZoomPos(coordinate);
+		this.cx = zoomPos[0];
+		this.cy = zoomPos[1];
+	};
+
+	this.getCursorXY = function(e) {
+		console.log("P:"+e.pageX);
+		var svgElementPos = document.querySelector("#zoom").getBoundingClientRect();
+		// var x = (window.Event) ? e.pageX : event.clientX + (document.documentElement.scrollLeft ? document.documentElement.scrollLeft : document.body.scrollLeft);
+		// var y = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+		var x = e.pageX - svgElementPos.left;
+		var y = e.pageY - svgElementPos.top;
+
+		var result = [x,y];
+		console.log(result);
+		return result;
+	};
+
+	this.getViewBox = function(coord, scale) {
+		var viewBox = [78742.3, -584111.1, 478020.8, 632482.1];
+		var width = 420;
+		var height = 556;
+
+		var clipX = viewBox[0] + (1 - 1/scale) * viewBox[2] * (coord[0] / width);
+		var clipY = viewBox[1] + (1 - 1/scale) * viewBox[3] * (coord[1] / height);
+		var clipW = viewBox[2]/scale;
+		var clipH = viewBox[3]/scale;
+
+		var newViewBox = [parseInt(clipX), parseInt(clipY), parseInt(clipW), parseInt(clipH)];
+
+		return newViewBox.join(' ');
+	};
+
+	this.getZoomPos = function(coord) {
+		var viewBox = [78742.3, -584111.1, 478020.8, 632482.1];
+		var width = 420;
+		var height = 556;
+
+		var x = viewBox[0] + viewBox[2] * coord[0] / width;
+		var y = viewBox[1] + viewBox[3] * coord[1] / height;
+
+		var result = [parseInt(x), parseInt(y)];
+
+		return result;
+	}
+});
